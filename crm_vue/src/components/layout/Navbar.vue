362fc965f93a8c54bcf767c805f44557b78bf5f0
Alex <alexmihoc@yahoo.com>
2022-10-18 10:30:31 +0300

    routing

--- crm_vue/src/components/layout/Navbar.vue
+++ crm_vue/src/components/layout/Navbar.vue
@@ -11,8 +11,14 @@
                 <router-link to="/dashboard/leads" class="navbar-item">Leads</router-link>
                 <div class="navbar-item">
                     <div class="buttons">
-                        <router-link to="/sign-up" class="button is-success"><strong>Sign up</strong></router-link>
-                        <router-link to="/log-in" class="button is-secondary">Log in</router-link>
+                        <template v-if="!$store.state.isAuthenticated">
+                            <router-link to="/sign-up" class="button is-success"><strong>Sign up</strong></router-link>
+                            <router-link to="/log-in" class="button is-secondary">Log in</router-link>
+                        </template>
+                        <template v-else>  
+                            <router-link to="/dashboard/my-account" class="button is-success">My Account</router-link>                      
+                            <button @click="logout" class="button is-secondary">Log out</button>
+                        </template>
                     </div>
                 </div>
             </div>
@@ -21,7 +27,24 @@
 </template>
 
 <script>
-export default {
-    name: 'Navbar'
-}
+import axios from 'axios'
+export default{
+        name: 'Navbar',
+        methods: {
+            async logout(){
+                await axios
+                    .post('/api/v1/token/logout')
+                    .then(response => {
+                        console.log('Logged out')
+                    })
+                    .catch(error => {
+                        console.log(JSON.stringify(error))
+                    })
+                axios.defaults.headers.common['Authorization'] = ''
+                localStorage.removeItem('token')
+                this.$store.commit('removeToken')
+                this.$router.push('/')
+            }
+        }
+    }
 </script>
---
+++ crm_vue/src/views/dashboard/Lead.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container">
+        <div class="columns is-multiline">
+            <div class="column is-12">
+                <h1 class="title">
+                    {{lead.title}}
+                </h1>
+                <router-link to="/" class="button is-light">Edit</router-link>
+            </div>
+            <div class="column is-6">
+                <div class="box">
+                    <h2>Details</h2>
+
+                </div>
+            </div>
+
+            <div class="column is-6">
+                <div class="box">
+                    <h2>Contact Information</h2>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default ({   
+    name:"Lead",
+    data() {
+        return{
+            lead: []
+        }
+    }
+})
+</script>
